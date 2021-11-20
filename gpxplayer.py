d3a1f8fa60fc585a19939323f14fd75583eec2ec
Carl Anthony P. Castueras <cpcastueras2@up.edu.ph>
2021-11-20 15:22:20 +0800

    added gpxplayer and sample gpx file

---
+++ gpxplayer.py
@@ -0,0 +1,21 @@
+import gpxpy
+import gpxpy.gpx
+from datetime import datetime
+import time
+
+# parse
+
+gpx_file = open('DS2-0420.gpx', 'r')
+
+gpx = gpxpy.parse(gpx_file)
+
+for track in gpx.tracks:
+    for segment in track.segments:
+        prevtime = segment.points[0].time
+        for point in segment.points:
+            currtime = point.time
+            delta = currtime - prevtime
+            time.sleep(delta.seconds)
+            print('({0},{1}) {2}'.format(point.latitude, point.longitude, point.time))
+            prevtime = currtime
+
