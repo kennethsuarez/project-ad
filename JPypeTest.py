a74fa6f20e9c6895f9524f05f00dd2edf34daac9
kennethsuarez <klsuarez1@up.edu.ph>
2022-02-14 04:33:08 +0800

    integrate predict to scheduler; log scheduler output to Output.txt

--- JPypeTest.py
+++ JPypeTest.py
@@ -2,29 +2,38 @@ import jpype
 import jpype.imports
 from jpype.types import *
 
+# read visited coordinates
+OUTPUT_PATH = 'output/visited.txt'
+with open(OUTPUT_PATH) as f:
+    visited_list = [x.rstrip() for x in f]
+#print(visited_list)
 
-jpype.startJVM(classpath=['/Users/Carlton/Documents/4th Year 1st Sem/CS 198199/project-ad/TTDM/target/classes'])
+# begin JPype operation
+jpype.startJVM(classpath=['TTDM/target/classes'])
 
 # print(jpype.java.lang.System.getProperty("java.class.path"))
 
 TTDM = jpype.JClass("com.mdm.sdu.mdm.model.taxi.TTDM_Taxi")
 
-jpype.java.lang.System.out.println("JPype")
+#jpype.java.lang.System.out.println("JPype")
  
 TTDM.train()
+idx = "20000005,"
+str = ",".join(visited_list)
+#print(idx+str)
+trajectory = jpype.java.lang.String(idx + str)
+#testString = jpype.java.lang.String("20000005,30$93@1618927642000,30$97@1618927900000,30$98@1618927919000,30$99@1618928000000," \
+#              "31$99@1618928003000,31$100@1618928039000,30$100@1618928059000,30$101@1618928072000," \
+#              "30$102@1618928119000,30$103@1618928138000,31$103@1618928178000,31$102@1618928238000")
 
-testString1 = jpype.java.lang.String("20000005,30$93@1618927642000,30$97@1618927900000,30$98@1618927919000,30$99@1618928000000," \
-             "31$99@1618928003000,31$100@1618928039000,30$100@1618928059000,30$101@1618928072000," \
-             "30$102@1618928119000,30$103@1618928138000,31$103@1618928178000,31$102@1618928238000")
+# testString2 = jpype.java.lang.String("20000005,30$104@1618928653000,30$103@1618928793000,30$102@1618928814000,"
+#                                     "30$101@1618928834000,31$101@1618928854000,31$100@1618928930000,"
+#                                     "31$99@1618928952000")
 
-testString2 = jpype.java.lang.String("20000005,30$104@1618928653000,30$103@1618928793000,30$102@1618928814000,"
-                                     "30$101@1618928834000,31$101@1618928854000,31$100@1618928930000,"
-                                     "31$99@1618928952000")
-
-output1 = TTDM.TTDM_Instance.predictHelper(testString1)
-output2 = TTDM.TTDM_Instance.predictHelper(testString2)
-
-print(testString1 + " predicted: " + output1)
-print(testString2 + " predicted: " + output2)
-
+#output = TTDM.TTDM_Instance.predictHelper(testString)
+# output2 = TTDM.TTDM_Instance.predictHelper(testString2)
+output = TTDM.TTDM_Instance.predictHelper(trajectory)
+#print(testString + " predicted: " + output)
+# print(testString2 + " predicted: " + output2)
+print(trajectory + " predicted: " + output)
 jpype.shutdownJVM()
--- scheduler.py
+++ scheduler.py
@@ -1,20 +1,28 @@
 import subprocess
 import os
 import json
+from datetime import datetime
+import time
 
-KFC_fence = [(120.99726, 14.55372), (120.99833, 14.55434)]
+text_file = open("Output.txt", "w")
+UPPER_LEFT_X = 120.90541
+UPPER_LEFT_Y = 14.785505
+DIFF_X = 0.002747
+DIFF_Y = 0.002657
 
 def checkGeoFence(point, x_pos, y_pos):
     x1, y1 = point
     if ((x1 < x_pos < x1+0.002747) and (y1-0.002657 < y_pos < y1)):
-        print("now inside priority  fence")
+        text_file.write("now inside priority fence\n")
+        text_file.flush()
         return True
     return False
     
 
 # round robin player
 def playVideo(queue, folderPath, video_points, x_pos, y_pos):
-    print("current queue: {0}".format(queue))
+    text_file.write("current queue: {0}\n".format(queue))
+    text_file.flush()
     video = queue.pop(0)    # remove from queue
 
     if video.endswith(".mp4"):
@@ -26,17 +34,21 @@ def playVideo(queue, folderPath, video_points, x_pos, y_pos):
                 video_points[video] += 2    # add extra point for geo-fenced locations
                 break
 
-        print("now playing: {0}".format(video))
+        text_file.write("now playing: {0}\n".format(video))
+        text_file.flush()
+
         omx = subprocess.run(["omxplayer", "-o", "local", path])    # play video
 
         queue.append(video)     # add played video to end of queue
 
     for video in queue:
-        print("{0}: {1} points".format(video, video_points[video]))
+        text_file.write("{0}: {1} points\n".format(video, video_points[video]))
+        text_file.flush()
 
-    print()
+    text_file.write("\n")
+    text_file.flush()
 
-
+# main code
 # begin by running gps supplier
 proc = subprocess.Popen(["python3", "-u", "gpxplayer.py"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
 
@@ -45,6 +57,8 @@ queue = []
 video_points = {}
 priority_zones = {}
 
+
+# setup priority zones with point counter
 priority_zones_json = open('priority_zones.json')
 priority_zones_data = json.load(priority_zones_json)
 
@@ -60,14 +74,32 @@ for ad in priority_zones_data['ads']:
 
 print(priority_zones)
 
+# setup and read visited coordinates
+OUTPUT_PATH = 'output/visited.txt'
+if not os.path.exists('output'):
+    os.makedirs('output')
 
 while len(queue) > 0:
     curr_loc = proc.stdout.readline().decode("utf-8")
     parsed_loc = curr_loc.split(" ")
     lon_lan = parsed_loc[0].split(",")
-    y_pos = lon_lan[0][1:]
-    x_pos = lon_lan[1][0:-1]
-    print(y_pos)
-    print("current location: {0}".format(curr_loc))
+    y_pos = float(lon_lan[0][1:])
+    x_pos = float(lon_lan[1][0:-1])
+    tim = datetime.fromisoformat(parsed_loc[1] + " " + parsed_loc[2].strip())
+    tim = int(tim.timestamp()*1000)
+
+    text_file.write("current location: {0}\n".format(curr_loc))
+    text_file.flush() 
+    # predict next location
+    x_grid =  (x_pos - UPPER_LEFT_X) // DIFF_X
+    y_grid =  (UPPER_LEFT_Y - y_pos) // DIFF_Y
+    coords = str(int(x_grid)) + "$" + str(int(y_grid)) + "@" + str(tim)
+    with open(OUTPUT_PATH, 'a+') as visited_list:
+        visited_list.write(coords + '\n')
+    pred = subprocess.Popen(["python3", "-u", "JPypeTest.py"], stdout=subprocess.PIPE)
+    predicted = pred.stdout.readline().decode("utf-8")
+    text_file.write(predicted+"\n")
+    # play video
     playVideo(queue, folderPath, video_points, float(x_pos), float(y_pos))
 
+text_file.close()
