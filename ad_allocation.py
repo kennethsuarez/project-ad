5e00b1cf0062a07af6ab192ba9c58fde91f8d10c
Carl Anthony P. Castueras <cpcastueras2@up.edu.ph>
2022-03-04 11:45:13 +0800

    modified ad_allocation to be incremental, new input files

--- ad_allocation.py
+++ ad_allocation.py
@@ -1,14 +1,29 @@
+from calendar import c
 import csv
 import functools
+import json
 
+UPPER_LEFT_X = 120.90541
+UPPER_LEFT_Y = 14.785505
+DIFF_X = 0.002747
+DIFF_Y = 0.002657
+
+def convertToGrid(coords):
+    lon, lat = coords
+    x_grid = (float(lon) - UPPER_LEFT_X) // DIFF_X
+    y_grid = (UPPER_LEFT_Y - float(lat)) // DIFF_Y
+    return str(int(x_grid)) + "$" + str(int(y_grid))
+
+
+################################### initial setup ###################################
 location_long_map = open("TTDM/output/location_long_map", "r")
 loc_long_dict = {}
 
 for x in location_long_map:
-    zone, long = x.split()
-    loc_long_dict[zone] = long
+    zone, long_z = x.split()
+    loc_long_dict[zone] = long_z
 
-print(loc_long_dict)
+#print(loc_long_dict)
 
 graph_dict = {}
 zone_min_dict = {}
@@ -20,7 +35,7 @@ with open("TTDM/output/graph/Taxi_graph.csv", newline="") as taxi_graph:
     zone_min_dict = {k: [] for k in range(1,int(number)+1)}
     next(graph)
     for row in graph:
-        print(row)
+        #print(row)
         graph_dict[int(row[0])].append((int(row[1]),float(row[2])))
 
 #print(graph_dict)
@@ -28,29 +43,57 @@ with open("TTDM/output/graph/Taxi_graph.csv", newline="") as taxi_graph:
 for src, content in graph_dict.items():
     zone_min_dict[src] = min(list(map(lambda x : x[1], content)))
 
-print(zone_min_dict)
+#print(zone_min_dict)
 
-zones = {k: [] for k in range(1,len(zone_min_dict)+1)}
+#zones = {k: [] for k in range(1,len(zone_min_dict)+1)}
 
-for abc in range(3):
-    zone = int(loc_long_dict[input()])
-    ad_len = float(input())
-    ad_name = input()
+################## input #########################
 
-    past_total = 0
-    if zones[zone]:
-        times = list(map(lambda x : x[0], zones[zone]))
-        past_total = functools.reduce(lambda a, b: a+b, times)
+#### load the zones ####
+priority_zones_json = open('priority_zones2.json')
+priority_zones = json.load(priority_zones_json)
+#print(priority_zones)
 
-    if past_total + ad_len < zone_min_dict[zone]:
-        print("ad {0} was ACCEPTED".format(ad_name))
-        print("time left: {0}".format(zone_min_dict[zone]-(past_total + ad_len)))
-        zones[zone].append((ad_len,ad_name))
-    else:
-        print("ad {0} was REJECTED".format(ad_name))
-        print("available time is {0}".format(zone_min_dict[zone] - past_total))
+#### load the ad lengths ####
+ad_list_json = open('ad_list.json')
+ad_lengths = json.load(ad_list_json)
 
-print(zones)
+#print(ad_lengths)
+
+#for abc in range(3):
+coords = input()
+ad_len = float(input())
+ad_name = input()
+
+past_total = 0
+if priority_zones.get(coords): #if that coordinate is a priority zone
+    for ad in priority_zones[coords]:
+        past_total += ad_lengths[ad]
+else:
+    priority_zones[coords] = []
+
+if past_total + ad_len < zone_min_dict[int(loc_long_dict[coords])]:
+    print("ad {0} was ACCEPTED".format(ad_name))
+    print("time left: {0}".format(zone_min_dict[int(loc_long_dict[coords])]-(past_total + ad_len)))
+    priority_zones[coords].append(ad_name+".mp4")
+    ad_lengths[ad_name+".mp4"] = ad_len
+else:
+    print("ad {0} was REJECTED".format(ad_name))
+    print("available time is {0}".format(zone_min_dict[int(loc_long_dict[coords])] - past_total))
+
+#print(priority_zones)
+#print(ad_lengths)
+
+
+#prep for output
+priority_zones_json.close()
+ad_list_json.close()
+
+with open('priority_zones2.json', 'w') as outfile:
+    json.dump(priority_zones, outfile)
+    
+with open('ad_list.json', 'w') as outfile:
+    json.dump(ad_lengths, outfile)
 
 
 
