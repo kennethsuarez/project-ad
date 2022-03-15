1acf60be74c4119199fe91037a5bf91314888961
Carl Anthony P. Castueras <cpcastueras2@up.edu.ph>
2022-03-15 14:21:14 +0800

    altered behavior on empty queue within zone, code cleanup

--- scheduler.py
+++ scheduler.py
@@ -20,14 +20,6 @@ UPPER_LEFT_Y = 14.785505
 DIFF_X = 0.002747
 DIFF_Y = 0.002657
 
-def checkGeoFence(point, x_pos, y_pos):
-    x1, y1 = point
-    if ((x1 < x_pos < x1+0.002747) and (y1-0.002657 < y_pos < y1)):
-        text_file.write("now inside priority fence\n")
-        text_file.flush()
-        return True
-    return False
-
 def convertToGrid(coords):
     lon, lat = coords
     x_grid = (float(lon) - UPPER_LEFT_X) // DIFF_X
@@ -99,18 +91,18 @@ def play_video(video, folderPath):
 
 def upc(video, video_points,play_counts,ad_list,coords): # could maybe make priority regions an input too
     priority_multiplier = 1
-    play_multiplier = 0.2 #arbitrary, 1 play outside zone is just equivalent to 0.2 plays
-    if priority_zones.get(coords): # if that coordinate does have a priority region
+    play_multiplier = 0.2 
+    if priority_zones.get(coords):
         if video in priority_zones[coords]: 
             text_file.write("in priority fence\n")
             text_file.flush()
-            priority_multiplier = 1    # We need to decide on how much of a premium this gives. May not be necessary
+            priority_multiplier = 1    # may not be necessary
             play_multiplier = 1
         
 
     new_count_utility =  math.log(ubs_utility_func(play_counts[video] + play_multiplier,ad_list[video]['count']))
     old_count_utility = math.log(ubs_utility_func(play_counts[video],ad_list[video]['count']))
-    count_utility_gained = (new_count_utility - old_count_utility) * 10000 #to put things in a workable range, but need to read the study more
+    count_utility_gained = (new_count_utility - old_count_utility) * 10000 #to put things in a workable range
 
     video_points[video] += (count_utility_gained * priority_multiplier)
     play_counts[video] += (1 * play_multiplier)
@@ -138,6 +130,10 @@ with open("TTDM/output/graph/Taxi_graph.csv", newline="") as taxi_graph:
     next(graph)
     for row in graph:
         graph_dict[int(row[0])].append((int(row[1]),float(row[2])))
+        #if float(row[2]) >= 5: #minimum threshold          # Actual values give worse fairness.
+        #    graph_dict[str(row[0]) + ">" +  str(row[1])] = float(row[2])
+        #else:
+        #    graph_dict[str(row[0]) + ">" + str(row[1])] = 5
 
 for src, content in graph_dict.items():
     zone_min_avg_dict[src] = min(list(filter(lambda x: x >= 0, map(lambda x: x[1], content)))) #temp fix for negs
@@ -154,7 +150,7 @@ nextQueue = []
 video_points = {}
 play_counts = {}
 reqd_counts = {}
-default_queue = [] #this will get modified, but will always be accessible via default_queue
+default_queue = [] # this will get modified, but will always be accessible via default_queue
 
 next_has_prio_ads = 0
 zone_has_prio_ads = 0
@@ -169,38 +165,26 @@ ad_list = json.load(ad_list_json)
 
 for filename in os.listdir(folderPath):
     default_queue.append(filename)
-    # start counter for each file
     video_points[filename] = 0
     play_counts[filename] = 0
-    #reqd_counts[filename] = 100 #temp value, should come from ad list later on
-    # How do we handle required play counts??? total, or in zone? Damn there's so much to consider
-    # we need a metric, like maybe how many equivalent play counts would it be in/out of the zone?
-
-    #ad_lengths[filename] = 6.0 # ^
 
 queue = default_queue.copy() #if rr we don't need to copy, since removed ad gets pushed back
 
-#reqd_counts['kfc.mp4'] = 100
-
 print(priority_zones)
 print(ad_list)
-#print(priority_zones["33$87"])
 
 # Begin JPype and train TTDM
 jpype.startJVM(classpath=['TTDM/target/classes'])
 TTDM = jpype.JClass("com.mdm.sdu.mdm.model.taxi.TTDM_Taxi")
 TTDM.train()
 
-# Empty initial sequence
 visited_list = []
 last_pred = ""
-
-
+update_next_queue = False 
 
 while len(queue) > 0: # might want to revisit this condition later
 
     ################### current coordinates input #######################
-    #curr_loc = proc.stdout.readline().decode("utf-8")  #deprecated
     curr_loc = ""
     while curr_loc == "":
         with open('output/gpx_loc.txt', 'r') as f:
@@ -229,7 +213,7 @@ while len(queue) > 0: # might want to revisit this condition later
             text_file.write("{0}\n".format(nextQueue))
             text_file.flush()
    
-            queue = nextQueue.copy() #hopefully no memory issues with this? garbage collected anyway
+            queue = nextQueue.copy()
             nextQueue = []
 
         if len(visited_list) != 0 and tim - int(visited_list[-1].split('@')[1]) > (120 * 1000):
@@ -251,14 +235,16 @@ while len(queue) > 0: # might want to revisit this condition later
     text_file.write(idx + visited_str + " predicted: " + str(predicted)+"\n")
     
     ###################  Video scheduling module ###################
-
-    # will need more modifications again, need to consider that UBS could create a queue that is not enough for the time.
-    # This is why the outer while loop may be problematic, considering setting it to just while true
     
-    # we can consider making this depend on arg i.e. if scheduler == "rr"
+    # This is why the outer while loop may be problematic, considering setting it to just while true
+    # We can consider making this depend on arg i.e. if scheduler == "rr"
 
-    #if no next queue, or prediction changed, generate a new next queue.
+     #if no next queue, or prediction changed, set to update next queue
     if not nextQueue or predicted != last_pred:
+        update_next_queue = True
+        
+
+    if update_next_queue:
         #nextQueue.clear() hopefully no memory issues, but yeah garbage collect
         tempQueue = generateNextQueue(predicted,default_queue)
         print("temp queue is")
@@ -267,7 +253,6 @@ while len(queue) > 0: # might want to revisit this condition later
             next_has_prio_ads = 1
 
         # we assume that the already scheduled playlist will finish i.e. be optimistic.
-        # optimistic allocator - this means we need accurate predictions for better performance.
         optimistic_counts = play_counts.copy()
         
         for ad in queue:
@@ -277,39 +262,35 @@ while len(queue) > 0: # might want to revisit this condition later
                 optimistic_counts[ad] += 0.2
                 
         zone_time = zone_min_avg_dict[int(loc_long_dict[predicted])]
-        nextQueue = ubs(tempQueue,optimistic_counts,ad_list,zone_time,next_has_prio_ads) # 60  is a temp value, connect it up
-        # not sure if it is still close to optimal if we delay it per zone, will need to investigate
+        #zone_time = graph_dict[loc_long_dict[coords] + ">" + loc_long_dict[predicted]]  #actual has worse fairness
+        nextQueue = ubs(tempQueue,optimistic_counts,ad_list,zone_time,next_has_prio_ads)
 
         text_file.write("populated next queue (" + str(predicted) + ")  with:\n")
-        text_file.flush()
-
         text_file.write("{0}\n".format(nextQueue))
-        text_file.flush()
-
         text_file.write("based on")
-        text_file.flush()
-
         text_file.write(str(optimistic_counts) + "\n")
         text_file.flush()
 
+        update_next_queue = False
+
     last_pred = predicted
         
-    # play video via scheduler output
-    
     text_file.write("current queue: {0}\n".format(queue))
     text_file.flush()
 
-    # get the top from the queue
     video = queue.pop(0)
     
-    # this is for round robin, we can make this depend on an arg later i.e. if scheduler == "rr"
-    #queue.append(video)
+    # for round robin, we can make this depend on an arg later i.e. if scheduler == "rr"
+    # queue.append(video)
 
-    # this is for UBS. A bit problematic, defeats the purpose of loc pred, leads to some unfairness...
-    # perhaps we can redo this?
+    # for UBS, generate new queue for current region if previous runs out
     if len(queue) == 0:
         tempQueue = generateNextQueue(coords,default_queue)
         queue = ubs(tempQueue,play_counts,ad_list,zone_time,zone_has_prio_ads)
+        
+        # over prediction case - when it runs out, update next queue optimistically
+        # comment out to do under prediction case
+        update_next_queue = True
 
     play_video(video,folderPath)
 
@@ -318,8 +299,7 @@ while len(queue) > 0: # might want to revisit this condition later
 
     for video in default_queue:
         text_file.write("{0}: {1} pts, {2}/{3} plays\n".format(video, video_points[video],play_counts[video],ad_list[video]['count']))
-        text_file.flush()
-
+        
     text_file.write("\n")
     text_file.flush() 
 
